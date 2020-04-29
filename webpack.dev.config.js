55140680b3e9081136a91927ef99c4a22753af28
angelia-yuqi-personal <greenfish84@126.com>
2020-04-29 16:34:12 +0800

    notice display conflux-rust version

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -21,12 +21,12 @@ module.exports = merge(commom, {
     proxy: {
       // '/proxy': 'http://127.0.0.1:3000',
       '/api': {
-        target: 'http://101.201.103.131:8885',
+        target: 'http://47.102.164.229:8885',
         // target: 'http://13.75.69.106',
         // pathRewrite: { '^/api': '' },
       },
       '/future': {
-        target: 'http://101.201.103.131:8885',
+        target: 'http://47.102.164.229:8885',
       },
       '/contract-manager': {
         target: 'http://101.201.103.131:8886',
